4cd917602576c574d296c50a1a807178288c8d75
Jose Fernando Morales <contacto@jfmorales.com>
2023-07-05 19:07:45 -0500

    update harpoon mappings

--- mappings.lua
+++ mappings.lua
@@ -39,10 +39,10 @@ return {
     -- Harpoon
     ["<leader>a"] = { function() require("harpoon.mark").add_file() end, desc = "Add file (Harpoon)" },
     ["<C-e>"] = { function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Quick Menu (Harpoon)" },
-    ["<C-h>"] = { function() require("harpoon.ui").nav_file(1) end, desc = "Go to File 1 (Harpoon)" },
-    ["<C-j>"] = { function() require("harpoon.ui").nav_file(2) end, desc = "Go to File 2 (Harpoon)" },
-    ["<C-k>"] = { function() require("harpoon.ui").nav_file(3) end, desc = "Go to File 3 (Harpoon)" },
-    ["<C-l>"] = { function() require("harpoon.ui").nav_file(4) end, desc = "Go to File 4 (Harpoon)" },
+    ["<C-1>"] = { function() require("harpoon.ui").nav_file(1) end, desc = "Go to File 1 (Harpoon)" },
+    ["<C-2>"] = { function() require("harpoon.ui").nav_file(2) end, desc = "Go to File 2 (Harpoon)" },
+    ["<C-3>"] = { function() require("harpoon.ui").nav_file(3) end, desc = "Go to File 3 (Harpoon)" },
+    ["<C-4>"] = { function() require("harpoon.ui").nav_file(4) end, desc = "Go to File 4 (Harpoon)" },
     -- Mine
     ["<leader>Q"] = { "<cmd>qa!<cr>", desc = "Quit All" },
   },
