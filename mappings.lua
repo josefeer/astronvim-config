a7935e64988abdd27681c6db436f20e512ca44bd
Jose Fernando Morales <jose.nearsure@impel.io>
2023-07-25 23:33:34 +0200

    rollback a bunch of stuff and added transparency

--- mappings.lua
+++ mappings.lua
@@ -21,8 +21,10 @@ return {
     ["<leader>T"] = { name = "Tabs" },
     ["<leader>Tn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>Tc"] = { "<cmd>tabclose<cr>", desc = "Close tab" },
+
     -- Telescope
     ["<leader>fR"] = { "<cmd>Telescope resume<cr>", desc = "Resume last search" },
+
     --- Buffer Nav
     ["<leader>b"] = { name = "Buffers" },
     ["<leader>be"] = { "<cmd>echo expand('%:p')<cr>", desc = "Where is this buffer?" },
@@ -36,13 +38,13 @@ return {
         end,
         desc = "Close buffer",
     },
-    -- Harpoon
-    ["<leader>a"] = { function() require("harpoon.mark").add_file() end, desc = "Add file (Harpoon)" },
-    ["<C-e>"] = { function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Quick Menu (Harpoon)" },
-    ["<C-1>"] = { function() require("harpoon.ui").nav_file(1) end, desc = "Go to File 1 (Harpoon)" },
-    ["<C-2>"] = { function() require("harpoon.ui").nav_file(2) end, desc = "Go to File 2 (Harpoon)" },
-    ["<C-3>"] = { function() require("harpoon.ui").nav_file(3) end, desc = "Go to File 3 (Harpoon)" },
-    ["<C-4>"] = { function() require("harpoon.ui").nav_file(4) end, desc = "Go to File 4 (Harpoon)" },
+    -- greatest remap ever (according to the primegen)
+    -- ["<leader>p"] = { "<cmd>_dP<cr>", desc = "Real Copy Paste" },
+
+    -- more primegen remaps
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+
     -- Mine
     ["<leader>Q"] = { "<cmd>qa!<cr>", desc = "Quit All" },
   },
--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,8 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.motion.harpoon" },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.motion.harpoon" },
+  -- { import = "astrocommunity.motion.marks-nvim" }
 }
